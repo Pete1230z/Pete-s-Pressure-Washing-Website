5950d186c37b07e569d02e3ff33411419bbe2ace
Nicholas Petree <petree234@gmail.com>
2024-08-19 19:28:31 -0500

    Lines 45-60: Added valMin and valMax to the constructors and set initial values.

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -93,11 +93,11 @@ public abstract class Part implements Serializable {
 
     public int getMin() { return valMin; }
 
-    public void setMin(int ) { this.valMin = ; }
+    //public void setMin(int ) { this.valMin = ; }
 
     public int getMax() { return valMax; }
 
-    public void setMax(int ) { this.valMax = ; }
+    //public void setMax(int ) { this.valMax = ; }
 
     public Set<Product> getProducts() {
         return products;
